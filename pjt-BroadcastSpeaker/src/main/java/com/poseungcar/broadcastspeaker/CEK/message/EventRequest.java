cb1157a2379dd01bf26bb7ce8d17e67a9433f307
나평근 <skvudrms54@gmail.com>
2020-01-16 16:20:29 +0900

    firt commit

---
+++ pjt-BroadcastSpeaker/src/main/java/com/poseungcar/broadcastspeaker/CEK/message/EventRequest.java
@@ -0,0 +1,15 @@
+package com.poseungcar.broadcastspeaker.CEK.message;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class EventRequest {
+
+	List<Map<String, String>> json = new ArrayList<Map<String, String>>();
+	
+	
+	
+	
+	
+}
