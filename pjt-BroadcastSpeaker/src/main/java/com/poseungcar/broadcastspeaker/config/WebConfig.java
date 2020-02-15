ca43134007bdf5f5a03be1e4cb52b4646aa7806a
나평근 <skvudrms54@gmail.com>
2020-02-15 15:38:16 +0900

    Reclassfy packages

---
+++ pjt-BroadcastSpeaker/src/main/java/com/poseungcar/broadcastspeaker/config/WebConfig.java
@@ -0,0 +1,49 @@
+package com.poseungcar.broadcastspeaker.config;
+
+import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	private static int MAX_FILE_ZIZE = 10 * 1024 * 1024;
+	
+	
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+	
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);	
+		
+		return new Filter[] {filter};
+	}
+	
+	@Override
+	   protected void customizeRegistration(Dynamic registration) {
+	      MultipartConfigElement multipartConfig = new MultipartConfigElement("/tmp/upload", MAX_FILE_ZIZE, MAX_FILE_ZIZE, 0);
+	      registration.setMultipartConfig(multipartConfig);
+	   }
+
+	
+}
