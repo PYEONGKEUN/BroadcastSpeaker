8ccf1710ca5ade0225f053d725a0bcfb8d311ceb
나평근 <>
2020-02-17 20:34:03 +0900

    Test CSS

--- pjt-BroadcastSpeaker/src/main/java/com/poseungcar/broadcastspeaker/controller/HomeController.java
+++ pjt-BroadcastSpeaker/src/main/java/com/poseungcar/broadcastspeaker/controller/HomeController.java
@@ -1,5 +1,15 @@
 package com.poseungcar.broadcastspeaker.controller;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -71,7 +81,8 @@ public class HomeController {
 					numberValue = (String) namesMap.get("value");
 				} 
 				mm= new MyExtensionMessage("call", numberValue +"번 손님" +placesValue + "로 와주세요.", true, "PlainText");
-
+				String msg = numberValue +"번 손님" +placesValue + "로 와주세요.";
+				ttsMP3(msg);
 				// Built-in Intent		처리
 			} else if (intentName.equals("Clova.YesIntent")) { 
 				mm = new MyExtensionMessage(intentName, "예 라고 하셨나요?", true, "PlainText");
@@ -100,6 +111,56 @@ public class HomeController {
         	remoteIp = request.getRemoteAddr();
         String result = "client addr : "+ remoteIp+":"+remotePort+"   remote Host : "+remoteHost;
 		return result;
+	}
+	
+	
+	
+	public void ttsMP3(String msg) {
+		String clientId = "YOUR_CLIENT_ID";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "YOUR_CLIENT_SECRET";//애플리케이션 클라이언트 시크릿값";
+        try {
+            String text = URLEncoder.encode(msg, "UTF-8"); // 13자
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/voice/v1/tts";
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+            // post request
+            String postParams = "speaker=mijin&speed=0&text=" + text;
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                InputStream is = con.getInputStream();
+                int read = 0;
+                byte[] bytes = new byte[1024];
+                // 랜덤한 이름으로 mp3 파일 생성
+                String tempname = Long.valueOf(new Date().getTime()).toString();
+                File f = new File("opt/clovatest/"+tempname + ".mp3");
+                f.createNewFile();
+                OutputStream outputStream = new FileOutputStream(f);
+                while ((read =is.read(bytes)) != -1) {
+                    outputStream.write(bytes, 0, read);
+                }
+                is.close();
+            } else {  // 오류 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                System.out.println(response.toString());
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 	}
 
 }
