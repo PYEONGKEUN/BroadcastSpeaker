a99a337b14b5be87a3a57b43be9b35f5c4e5b28d
나평근 <skvudrms54@gmail.com>
2020-02-21 00:05:20 +0900

    Change some code in LoginController

--- pjt-BroadcastSpeaker/src/main/java/com/poseungcar/broadcastspeaker/controller/LoginController.java
+++ pjt-BroadcastSpeaker/src/main/java/com/poseungcar/broadcastspeaker/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.poseungcar.broadcastspeaker.controller;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.poseungcar.broadcastspeaker.DTO.Member;
 import com.poseungcar.broadcastspeaker.service.IMemberService;
@@ -63,12 +67,16 @@ public class LoginController {
 	}
 	//안드로이드에서 사용할 로그인 
 	
-	
+	@ResponseBody
 	@RequestMapping(value = "/login.and.action", method = RequestMethod.POST)
-	public String loginAndAction(
+	public Map<Object, String> loginAndAction(
 			HttpServletRequest request, 
 			HttpSession session, 
 			@CookieValue(value="storeIdCookie", required = false) Cookie cookie ) {
+		
+
+		Map<Object, String> result = new HashMap<Object,String>();
+		
 		logger.info("Do Login");
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
@@ -76,13 +84,17 @@ public class LoginController {
 		Member member = new Member();
 		
 		member.setMem_id(id);
-		member.setMem_pw(pw);				
+		member.setMem_pw(pw);
+		
 
 		if(memberService.loginCookie(member, cookie)) {
-			return "login-result";
+            session.setAttribute("memberinfo", member);
+            result.put(member, "true");
 		}else {
-			return "login";
+            session.invalidate(); //로그인 성공시에만 세션 아이디가 안드로이드로 넘어감.
+            result.put(null, "false");
 		}		
+		return result;
 		
 	}
 	
